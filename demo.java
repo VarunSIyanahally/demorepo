dc038c3dc208ea3bd13c84e4ac8ae2130e393fd1
Varun S Iyanahally <vvce23cse0010@vvce.ac.in>
2025-10-31 03:29:27 +0530

    update demo

--- demo.java
+++ demo.java
@@ -1,5 +1,7 @@
 public class demo {
     public static void main (String[]args)
-    system.out.println("hello i am varun")
-    system.out.println("version 1")
+    system.out.println("hello i am varun");
+    system.out.println("version 1");
+    system.out.println("version 2");
+
 }
